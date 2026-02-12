f448f2c1b7842ea63a4eb5dcc44a4f9f93911c61
Bean080 <bclarkewiz@gmail.com>
2026-02-12 15:56:54 -0600

    Completed routing

--- src/app.jsx
+++ src/app.jsx
@@ -2,22 +2,47 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Lobby } from './start/lobby.jsx';
+import { Game } from './game/game.jsx';
+import { End } from './finish/end.jsx';
+import { Account } from './account/account.jsx';
+
+
 export default function App() {
   return (
-    <body>
+    <BrowserRouter>
+        <body>
         <div class="vignette"></div>
-
-        <header>
-            <h1>Photogenic</h1>
+            <header>
+            <nav className="navbar fixed-top navbar-dark">
+                <h1>Photogenic</h1>
+                <menu className="navbar-nav">
+                  <li className="nav-item">
+                    <NavLink className='nav-link' to='account'>Account</NavLink>
+                  </li>
+                </menu>
+            </nav>
+            </header>
             <h5 id="code">Game Code: ####</h5>
-            <a href="account_page.html" class="styled_button">Account</a>
-        </header>
+            
 
-        <main>App Here</main>
+            <Routes>
+                <Route path='/' element={<Lobby />} exact />
+                <Route path='/game' element={<Game />} />
+                <Route path='/end' element={<End />} />
+                <Route path='/account' element={<Account />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
 
-        <footer>
-            <a id="code" href="https://github.com/Bean080/CS260_Website/tree/main">Benjamin Clarke - GitHub (clickhere)</a>
-        </footer>
-    </body>
+            <footer>
+                <a id="code" href="https://github.com/Bean080/CS260_Website/tree/main">Benjamin Clarke - GitHub (clickhere)</a>
+            </footer>
+        </body>
+    </BrowserRouter>
   );
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
